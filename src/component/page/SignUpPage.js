adf68930d0e5ccad5d957ef76b8cf545f26a7d20
신은수 <ses2201@naver.com>
2022-11-23 18:39:44 +0900

    [fix] ui

--- src/component/page/SignUpPage.js
+++ src/component/page/SignUpPage.js
@@ -76,10 +76,10 @@ function SignUpPage({navigation}) {
 
             <View style={{...styles.signUpForm, paddingTop: Platform.OS === "ios" ? 0 : 30}}>
                 <StyledInputContainer>
-                    <CustomInput placeholder={"LoginID"} width={Dimensions.get('window').width * 0.45} height={"60px"}
+                    <CustomInput placeholder={"LoginID"} width={Dimensions.get('window').width * 0.46} height={"60px"}
                                  name={"loginId"}
                                  value={value.loginId} onChange={onChange}/>
-                    <View style={{marginLeft: Dimensions.get('window').width * 0.05}}>
+                    <View style={{marginLeft: Dimensions.get('window').width * 0.04}}>
                         <CustomButton type={"b"} width={Dimensions.get('window').width * 0.3} height={"50px"}
                                       borderRadius={50}
                                       pointColor={colors.pointBlue}
